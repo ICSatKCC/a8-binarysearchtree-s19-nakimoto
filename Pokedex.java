8cb7f4a601be34822bdd267ecd85fb4373b28d55
Nanami Kimoto <nkimoto@hawaii.edu>
2019-05-07 22:43:21 -1000

    A8 repush
    Professor, I saw your comment that my code wasn't there. I'm not sure what happened? I also noticed Kristine was removed as a coding reviewer even though I got her review. I was pretty sure I pushed my code when it was due. It had to be done anyways for A9.
    I've pushed this in case you want to take a look at it. Sometimes my computer or gitkraken is weird. I didn't realise anything until I saw your comment.

--- Pokedex.java
+++ Pokedex.java
@@ -1,60 +1,87 @@
+import java.util.*;
+import java.util.Random;
 /**
- * PokeDex Driver. 
- * 
- * @author Nanami Kimoto
- */
- //driver base code.
-   public class Pokedex {   
+* Pokedex.
+* @author Nanami Kimoto
+* @since 4/20/19
+*/
+public class Pokedex {
+
+   /**Scanner for input.*/
+   private static Scanner userIn = new Scanner(System.in);
+   /**String for input.*/
+   private static String inString = new String("");
+   /**Random.*/
+   private static Random randGen = new Random();
+   /**For menu design.*/
+   private static String b = "*************";
+   /**PokeTree.*/
+   private static PokeTree<Pokemon> tree = new PokeTree<Pokemon>();
+   /**Object declaration constant.*/
+   private static Pokemon p;
+   /**Private int declaration.*/
+   private static int Count = 0;
    
-   /**
-    * Driver code to test class
-    * 
-    * @param commandlineArguments
-    *            are not used
-    */
-      public static void main(String[] commandlineArguments) {
-      //Use PokeNode<String>.
-         BinarySearchTree<String> tree = new BinarySearchTree<String>();
-         System.out.println("TEST add() method:");
-         tree.add("mahimahi");
-         tree.add("hee");
-         tree.add("ono");
-         tree.add("mano");
-         tree.add("lauwiliwili");
-         tree.add("honu");
-         tree.add("ulua");
-         tree.add("uhu");
-         tree.add("ahi");
-         System.out.println("preorder:\n" + tree.preOrder());
-         System.out.println("inorder:\n" + tree.toString());
-         System.out.println("postorder:\n" + tree.postOrder());
-      
-      //testing get method.
-         System.out.println("TEST get() method:");
-         String fish = tree.get("mahimahi");
-         System.out.println(fish);
-         fish = tree.get("ono");
-         System.out.println(fish);
-         try {
-            fish = tree.get("tuna");
-            System.out.println(fish);
-         } 
-            catch (TreeException exception) {
-               System.out.println(exception.toString());
-            }
-         fish = tree.get("uhu");
-         System.out.println(fish);
-      
-      //Testing remove method. 
-         System.out.println("\nTEST remove() method:");
-         System.out.println("inorder:\n" + tree.toString());
-         tree.remove("mahimahi");
-         System.out.println("inorder:\n" + tree.toString());
-         tree.remove("ulua");
-         System.out.println("inorder:\n" + tree.toString());
-         tree.remove("ono");
-         System.out.println("inorder:\n" + tree.toString());
-         tree.remove("ahi");
-         System.out.println("inorder:\n" + tree.toString());
-      }// end of main
-   }// end of class
+   /** Main Method.
+   *@param args not used.
+   */
+   
+   public static void main(String[] args) { 
+   //menu.
+   //endloop for menu use. 
+      boolean endLoop = false;
+      while (!endLoop) {
+         
+         System.out.println("This is your Pokedex.");
+         System.out.println("1. Add Pokemon");
+         System.out.println("2. Trade Pokemon");
+         System.out.println("3. Print Pokedex");
+         System.out.println("0. Exit the program");
+         System.out.println("What would you like to do?");
+         
+         
+         inString = userIn.nextLine().trim();
+         System.out.println(b); 
+         //switch for menu options.      
+         switch(inString) {
+         
+         //exit condition.
+            case "0": endLoop = true; 
+               System.out.println("Good bye!");
+               break;
+               
+            case "1": 
+            //adds pokemon.
+               System.out.println("Catch Pokemon"); 
+               p = ChoosePokemon.choosePokemon();
+               tree.add(p);
+               Count++;
+               break;
+               
+            case "2": 
+            //removes pokemon.
+               p = ChoosePokemon.choosePokemon();
+               tree.remove(p);
+               Count--;
+               break;
+               
+            case "3": 
+                //prints caught pokemon.
+               System.out.println("Pokemon caught: " + Count + "\n");
+               tree.printPokeTree();
+               System.out.println("\n" + b);
+               break;
+               
+            default: 
+            //invalid entry.
+               System.out.println("\n Invalid entry!");
+               System.out.println("Please enter a 0, 1, 2 or 3 ");
+               break;
+               
+         }    
+      }  
+
+
+   }  //close main
+
+} //close class
